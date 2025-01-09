e41e00c13bb1e4b3d9fa18ac10335737b0b50100
hamada <a@hama.day>
2025-01-09 10:05:42 +0900

    return dynamic content

--- http_server.go
+++ http_server.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
 )
+
+type Content struct { // テンプレート展開用のデータ構造
+	Title string
+	Count int
+}
+
+func responseHandler(w http.ResponseWriter, r *http.Request) {
+	content := Content{"Hello World.", 1}
+	tmpl, err := template.ParseFiles("views/layout.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(w, content)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // ref: https://qiita.com/taizo/items/bf1ec35a65ad5f608d45
 // $ ./http_server
 // $ curl http://localhost:8080
 func main() {
-	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.HandleFunc("/", responseHandler)
 	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
 }
