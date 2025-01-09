7cfd18bf4f8b1989774a4e1b72f012cb77799d32
hamada <a@hama.day>
2025-01-09 09:41:46 +0900

    add my first http server

--- http_server.go
+++ http_server.go
@@ -1,15 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+// ref: https://qiita.com/taizo/items/bf1ec35a65ad5f608d45
+// $ ./http_server
+// $ curl http://localhost:8080
 func main() {
-	printHello()
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":8080", nil)
 }
 
-func printHello() {
-	fmt.Printf(Hello())
-}
-
-func Hello() string {
-	return fmt.Sprintf("Hello, %s!\n", "http_server")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
 }
--- http_server_test.go
+++ http_server_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, http_server!\n"
+	// got := Hello()
+	// want := "Hello, http_server!\n"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	// if got != want {
+	// t.Errorf("got %q, want %q", got, want)
+	// }
 }
